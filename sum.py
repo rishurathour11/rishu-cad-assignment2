b70f60918dd63619687356db250b2497297fc3c3
rishurathour11 <rishurathour1449@gmail.com>
2026-03-24 21:42:56 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Take input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Calculate sum
+sum = num1 + num2
+
+# Display result
+print("The sum is:", sum)
