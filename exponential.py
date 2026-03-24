a9e5628ea117b3fdcf934a2d6758b222ed6bc942
rishurathour11 <rishurathour1449@gmail.com>
2026-03-24 21:47:52 +0530

    Create exponential.py

---
+++ exponential.py
@@ -0,0 +1,9 @@
+# Take input from user
+base = float(input("Enter base number: "))
+exponent = float(input("Enter exponent: "))
+
+# Calculate power
+result = base ** exponent
+
+# Display result
+print("The result is:", result)
