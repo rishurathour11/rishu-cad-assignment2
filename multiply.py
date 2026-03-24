52fc24038d817246cd95f71da55917e800c6066d
rishurathour11 <rishurathour1449@gmail.com>
2026-03-24 21:44:52 +0530

    Add multiplication functionality in multiply.py

---
+++ multiply.py
@@ -0,0 +1,9 @@
+# Take input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Perform multiplication
+result = num1 * num2
+
+# Display result
+print("The result of multiplication is:", result)
